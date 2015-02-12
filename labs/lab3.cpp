30ca466c13a31100cfcf6447c3d89f838a0e19b8
Ryan Roberge <roberger@wit.edu>
2015-02-11 21:38:59 -0500

    Lab 3 is done.

--- labs/lab3.cpp
+++ labs/lab3.cpp
@@ -1,10 +1,14 @@
+// Ryan Roberge
+// Professor Lawrance
+// 2-11-2015
+// Lab 3 - Concentration
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 using namespace std;
-
-enum State { FIRST, MATCH, NO_MATCH };
 
 // To clear the screen, look up ANSI escape codes
 // Concentration game model
@@ -24,14 +28,12 @@ public:
     // Return visible stuff (invisible stuff is shown with character *)
     char get(int row, int column);
     // Flip this row/column
-    void flip(int row, int column);
+    void flip(int row, int column, Model * model);
     // Is the game over?
-    bool gameOver();
+    bool gameOver(Model * model);
 private:
     // Is the row/column valid?
     bool valid(int row, int column);
-    // Did the cell at current row/column match the cell at the last row/column 
-    bool matched(int row, int column);
     // Fields (member data)
     // Randomly generated grid. This has pairs of characters in it
     char ** grid;
@@ -42,9 +44,8 @@ private:
     // What's the height?
     int height;
     // What'd we flip last?
-    int lastRow;
-    int lastColumn;
-    State state;
+    vector<int> lastRows;
+    vector<int> lastColumns;
 };
 
 // Show (output) the state of the model
@@ -76,9 +77,8 @@ private:
 Model::Model(int w, int h) {
     width = w;
     height = h;
-    lastRow = -1;
-    lastColumn = -1;
-    state = FIRST;
+	lastRows.push_back(-1); // Initializes the lastRows vector to something that isn't empty
+    lastColumns.push_back(-1); // Initializes the lastColumns vector to something that isn't empty
     grid = new char*[height];
     visible = new char*[height];
     // For every row, create the array for that row
@@ -90,13 +90,49 @@ Model::Model(int w, int h) {
     // TODO: make this random-ish
     // Look at asciitable.com and do some stuff with rand() % number
     // Hint: insert characters in order, then shuffle later in a separate loop
-    for (int i = 0; i < height; i++) {
+    
+	char letter = 'A';
+	for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            grid[i][j] = 'a';
+            grid[i][j] = letter;
             // Everything's invisible at first
             visible[i][j] = '*';
+			if(j % 2 == 1)
+				letter++;
+			if(letter > 'Z')
+				letter = 'A';
         }
     }
+	
+	int otheri, otherj;
+	for(int i = 0; i < height; i++)
+	{
+		for(int j = 0; j < width; j++)
+		{
+			otheri = rand() % height;
+			otherj = rand() % width;
+			
+			letter = grid[i][j];
+			grid[i][j] = grid[otheri][otherj];
+			grid[otheri][otherj] = letter;
+			
+		}
+	}
+//////////////////////////////////////////////////////
+// Displays the contents of grid (cheat so we can see)
+	    for (int j = 0; j < width; j++) {
+        cout << "\t" << j;
+    }
+    cout << endl;
+    for (int i = 0; i < h; i++) {
+        cout << i;
+        for (int j = 0; j < w; j++) {
+            cout << "\t" << grid[i][j];
+        }
+        cout << endl;
+    }
+	cout << endl;
+//////////////////////////////////////////////////////
 }
 // Destructor deletes dynamically allocated memory
 Model::~Model() {
@@ -107,42 +143,115 @@ Model::~Model() {
     delete grid;
     delete visible;
 }
-// TODO: Is the row/column valid?
-// That is, is the row within the height, and is the column within the width?
-// Return whether it is or isn't.
-bool Model::valid(int row, int column) {
-    return true;
-}
-bool Model::matched(int row, int column) {
-    return true;
-}
-// TODO: Flip a cell
-void Model::flip(int row, int column) {
-    // If the row and column are not valid, break out and don't do anything
-    if (!valid(row, column)) { return; }
-    
-    // If the last selected row and column are invalid,
-        // It means we're selecting the first "cell" to flip
-    // Otherwise, we are selecting the next "cell" to flip
-        // If the last cell and the current cell match, great!
-        // Otherwise, make the last cell invisible (set it to *)
-    // Make the current cell visible
-}
-// TODO: If everything is visible, then it's game over
-bool Model::gameOver() {
-    // Hint: assume the game is over, unless it isn't
-    // Hint: Loop through the grid and see if any element is not visible
-    return false;
-}
+
 int Model::getWidth() {
     return width;
 }
 int Model::getHeight() {
     return height;
 }
+
+// TODO: If everything is visible, then it's game over
+bool Model::gameOver(Model * model) {
+    for(int r = 0; r < model->getHeight(); r++)
+	{
+		for(int c = 0; c < model->getWidth(); c++)
+		{
+			if(visible[r][c] == '*')
+				return false;
+		}
+	}
+	return true;
+}
+
 char Model::get(int row, int col) {
     return visible[row][col];
 }
+// TODO: Is the row/column valid?
+// That is, is the row within the height, and is the column within the width?
+// Return whether it is or isn't.
+bool Model::valid(int row, int column) {
+	
+	if(row > getWidth()-1 || row < 0)
+	{
+		cout << endl << "Coordinates are out of range, please try again." << endl << endl;
+		return false;
+	}
+	
+	if(column > getHeight()-1 || column < 0)
+	{
+		cout << endl << "Coordinates are out of range, please try again." << endl << endl;
+		return false;	
+	}
+
+    return true;
+}
+
+// TODO: Flip a cell
+void Model::flip(int row, int column, Model * model) {
+		
+    // If the row and column are not valid, break out and don't do anything
+    if (!valid(row, column)) { return; }
+	// Ensures the row and column given this time is not the same as the previous set
+	else if(row == lastRows.back() && column == lastColumns.back())
+	{
+		cout << endl << "Please enter a different row and column than the previous." << endl << endl;
+		return;
+	}
+	// Ensures the row and column given is not an already flipped cell
+	else if(visible[row][column] != '*')
+	{
+		cout << endl << "Please pick cells that haven't already been flipped." << endl << endl;
+		return;	
+	}
+    // If the last selected row and column are invalid,
+        // It means we're selecting the first "cell" to flip
+		else if(lastRows.back() == -1 && lastColumns.back() == -1)
+		{
+			visible[row][column] = grid[row][column];
+			lastRows.push_back(row);
+			lastColumns.push_back(column);
+			return;
+		}
+    // Otherwise, we are selecting the next "cell" to flip
+        // If the last cell and the current cell match, great!
+		else if(grid[row][column] == grid[lastRows.back()][lastColumns.back()])
+		{
+			cout << endl << "[MATCH]"  << endl << endl;
+			visible[row][column] = grid[row][column];
+			lastRows.push_back(-1);
+			lastColumns.push_back(-1);
+			return;
+		}			
+        // Otherwise, make the last cell invisible (set it to *)
+		// No match case
+		else if(grid[row][column] != grid[lastRows.back()][lastColumns.back()])
+		{
+			visible[row][column] = grid [row][column];
+			
+			// Copied show() function code, needed because view is not defined in this scope
+			for (int j = 0; j < model->getWidth(); j++) {
+					cout << "\t" << j;
+			}
+			cout << endl;
+			for (int i = 0; i < model->getHeight(); i++) {
+				cout << i;
+				for (int j = 0; j < model->getWidth(); j++) {
+					cout << "\t" << model->get(i, j);
+						}
+					cout << endl;
+				}
+				
+			cout << endl << "[NO MATCH]"  << endl << endl;
+			
+			visible[row][column] = '*';
+			visible[lastRows.back()][lastColumns.back()] = '*';
+			lastRows.push_back(-1);
+			lastColumns.push_back(-1);
+			return;
+		} 
+}
+
 // Show the model, one cell at a time
 void View::show(Model * model) {
     for (int j = 0; j < model->getWidth(); j++) {
@@ -163,18 +272,37 @@ void View::show(Model * model) {
 // Until we're done
 void Controller::loop() {
     int row, col;
-    while (!model->gameOver()) {
+    while (!model->gameOver(model)) {
         view->show(model);
-        cout << "Enter row:    ";
+        cout << endl << "Enter row:    ";
         cin >> row;
+		// Handy break out of game case for ease of testing
+		if(row == -1)
+		{
+			delete model;
+			delete view;
+			exit(0);
+		}
         cout << "Enter column: ";
+		// Handy break out of game case for ease of testing
         cin >> col;
-        model->flip(row, col);
+		if(col == -1) 
+		{
+			delete model;
+			delete view;
+			exit(0);
+		}
+        model->flip(row, col, model);
     }
-    cout << "Hooray, you win!" << endl;
+    
+	view->show(model);
+	cout << endl << "Hooray, you win!"<< endl;
+	return;
+
 }
 
 int main() {
     Controller c;
     c.loop();
+	return 0;
 }
